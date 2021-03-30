18d7df257a62229778134375dce94aa59c7b8ccc
theFellandes <57584110+theFellandes@users.noreply.github.com>
2021-03-30 16:58:38 +0300

    Add files via upload
    weird commit for classes in python

---
+++ tetris.py
@@ -0,0 +1,14 @@
+class Tetris:
+
+    def __init__(self):
+        self.main()
+
+    def tetris(self):
+        print("yay")
+
+    def main(self):
+        self.tetris()
+
+
+if __name__ == '__main__':
+    tetris = Tetris()

76248bc99ad127da2b2513d7316bb7cfd719108f
Ahmet F. Turhan <afaturhan@gmail.com>
2021-04-10 17:27:06 +0300

    can be rotated needs to be fixed

--- Tetris_2048.py
+++ Tetris_2048.py
@@ -82,8 +82,8 @@ def start():
 # Function for creating random shaped tetrominoes to enter the game grid
 def create_tetromino(grid_height, grid_width):
     # type (shape) of the tetromino is determined randomly
-    # tetromino_types = [ 'Z' ]
     tetromino_types = ['I', 'O', 'Z', 'S', 'L', 'J', 'T']
+    #tetromino_types = ['I', 'O', 'Z', 'S', 'L', 'J', 'T']
     random_index = random.randint(0, len(tetromino_types) - 1)
     random_type = tetromino_types[random_index]
     # create and return the tetromino
